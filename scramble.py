a4b4cc26063c2113c0fbf31acbeeeab219a60488
Devon Mars <dbharris2@gmail.com>
2023-09-30 21:47:50 -0600

    Create letters file

--- scramble.py
+++ scramble.py
@@ -1,39 +1,11 @@
 from board import Board
 from dictionary import EnglishDictionary
+from letters import letters
 
 dictionary = EnglishDictionary()
 board = Board()
 board_size = 4
 results = {}
-
-letters = {
-    "a": 1,
-    "b": 4,
-    "c": 4,
-    "d": 2,
-    "e": 1,
-    "f": 4,
-    "g": 3,
-    "h": 3,
-    "i": 1,
-    "j": 10,
-    "k": 5,
-    "l": 2,
-    "m": 4,
-    "n": 2,
-    "o": 1,
-    "p": 4,
-    "q": 10,
-    "r": 1,
-    "s": 1,
-    "t": 1,
-    "u": 2,
-    "v": 5,
-    "w": 2,
-    "x": 8,
-    "y": 3,
-    "z": 1,
-}
 
 
 def find_word(word, word_index, row, col, value):
